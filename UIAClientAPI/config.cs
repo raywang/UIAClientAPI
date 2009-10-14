588da7f866fe09f75fbd34fa6f1a07a7e9a868fc
Felicia Mu <fxmu@novell.com>
2009-10-14 18:36:58 +0800

    add config utils class

---
+++ UIAClientAPI/config.cs
@@ -0,0 +1,60 @@
+﻿// Config.cs: save the configuration for UIAClinet test
+//
+// Author:
+//   Felicia Mu  (fxmu@novell.com)
+//
+// Copyright (c) 2009 Novell, Inc (http://www.novell.com)
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UIAClientAPI
+{
+	class Config
+	{
+		// whether or not to take screenshots
+		private bool takeScreenShots = true;
+		public bool TakeScreenShots
+		{
+			get { return this.takeScreenShots; }
+			set { this.takeScreenShots = value; }
+		}
+
+		// where to write procedure logger output, screenshots, etc
+		private string output_dir = " ";
+		public string Output_Dir
+		{
+			get { return this.output_dir; }
+			set { this.output_dir = value; }
+		}
+
+		//  the shotr time to delay
+		private string short_delay = "0.5";
+		public string Short_Delay
+		{
+			get { return this.short_delay; }
+			set { this.short_delay = value; }
+		}
+
+		//  the middle time to delay
+		private string medium_delay = "4";
+		public string Medium_Delay
+		{
+			get { return this.medium_delay; }
+			set { this.medium_delay = value; }
+		}
+
+		//  the long time to delay
+		private string long_delay = "10";
+		public string Long_Delay
+		{
+			get { return this.long_delay; }
+			set { this.long_delay = value; }
+		}
+
+
+	}
+}
---
+++ UIAClientAPI/utils.cs
@@ -0,0 +1,47 @@
+﻿// Utils.cs: Utility functions
+//
+// Author:
+//   Felicia Mu  (fxmu@novell.com)
+//
+// Copyright (c) 2009 Novell, Inc (http://www.novell.com)
+//
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace UIAClientAPI
+{
+	class Utils
+	{
+		//'Takes a screenshot of the desktop
+		public void takeScreenshot (string path)
+		{
+			Config config = new Config ();
+			// pause before taking screenshots, otherwise we get half-drawn widgets
+			Thread.Sleep (config.TakeScreenShots);
+
+			/*
+			 * assert os.path.isdir(os.path.dirname(path))
+
+    fileExt = os.path.splitext(path)[1][1:]
+
+    rootWindow = gtk.gdk.get_default_root_window()
+    geometry = rootWindow.get_geometry()
+    pixbuf = gtk.gdk.Pixbuf(gtk.gdk.COLORSPACE_RGB, False, 8, geometry[2], geometry[3])
+    gtk.gdk.Pixbuf.get_from_drawable(pixbuf, rootWindow, rootWindow.get_colormap(), 0, 0, 0, 0, geometry[2], geometry[3])
+
+    # gtk.gdk.Pixbuf.save() needs 'jpeg' and not 'jpg'
+    if fileExt == 'jpg': fileExt = 'jpeg'
+
+    try:
+        pixbuf.save(path, fileExt)
+    except gobject.GError:
+        raise ValueError, "Failed to save screenshot in %s format" % fileExt
+
+    assert os.path.exists(path)
+			 */
+		}
+	}
+}
